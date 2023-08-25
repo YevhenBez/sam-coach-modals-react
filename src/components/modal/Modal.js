47009cf2eeb874fdf8db176d36670c6662f0cd62
YevhenBez <byxgalter@bigmir.net>
2023-08-25 21:38:12 +0300

    Использовал метод Реакта createPortal и вернул разметку модального Окна. Также прописал немного стилей для модалки.  Тренировка с Модалками в React
    Использовал метод Реакта    createPortal    и вернул разметку модального Окна.  Для этого в файле    Modal.js   импортнул метод Реакта    createPortal   при помощи   import { createPortal } from 'react-dom';    также импортнул стили. Затем вернул разметку модалки при помощи данного метода  использовав команду    return createPortal( )   Внутри которой помимо разметки Модалки (форма, инпуты, кнопка), я вставил разметку, которая позволяет быть Модалке сверху всех окон, а именно   modalRoot    В файле   modal.module.css    просто прописал стили для данной модалки. Тренировка с Модалками в React

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,5 +1,37 @@
 import React from 'react';
 
+import { createPortal } from 'react-dom';
+
+import css from './css/modal.module.css';
+
 const modalRoot = document.querySelector('#modalRoot');
 
-const Modal = () => { };
+const Modal = () => {
+    return createPortal(
+    <div  className={css.overlay}>
+      <div className={css.modal}>
+        <form className={css.formStyle} >
+      <label className={css.labelStyle}>
+        Name
+        <input
+          className={css.inputName}
+          type="text"
+          
+        />
+      </label>
+      <label className={css.labelStyle}>
+        Email
+        <input
+          className={css.inputName}
+          type="email"
+        />
+      </label>
+      <button type="submit" className={css.btnAdd}>
+        Add contact
+      </button>
+    </form>
+      </div>
+    </div>,
+    modalRoot
+  );
+};
